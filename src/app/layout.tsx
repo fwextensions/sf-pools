17d7c9131070309cfb5a6f5d35494e5ecbd8e3d0
John Dunning <fw@johndunning.com>
2025-09-20 11:31:34 -0700

    Add favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,17 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className="h-full">
-			<body className="min-h-screen bg-white text-slate-900 antialiased">
-				{children}
-			</body>
+		<head>
+			<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+			<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+			<link rel="shortcut icon" href="/favicon.ico" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+			<meta name="apple-mobile-web-app-title" content="SF Pools" />
+			<link rel="manifest" href="/site.webmanifest" />
+		</head>
+		<body className="min-h-screen bg-white text-slate-900 antialiased">
+			{children}
+		</body>
 		</html>
 	);
 }
